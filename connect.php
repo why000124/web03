283ac2380c16448eadd5ab8d9401be926e40a7e7
why000124 <tomastwo52@gmail.com>
2024-09-26 17:40:13 +0700

    php add

---
+++ connect.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "college";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>connect</title>
+</head>
+<body>
+    
+</body>
+</html>
