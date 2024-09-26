26c903812796f673de65da4c74c52e34f4bf2d11
why000124 <tomastwo52@gmail.com>
2024-09-26 17:54:35 +0700

    ควย

--- connect.php
+++ connect.php
@@ -1,14 +1,15 @@
 <?php
-$servername = "localhost";
+$servername = "college";
 $username = "root";
 $password = "";
 $dbname = "college";
+
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
 ?>
